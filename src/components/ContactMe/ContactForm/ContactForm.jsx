dfa417404754c9b69c7a19dae00e7d7519c47d37
nicolasviruel <nicolasviruel@gmail.com>
2024-04-09 13:08:01 -0300

    Se arreglan los div de contactForm

--- src/components/ContactMe/ContactForm/ContactForm.jsx
+++ src/components/ContactMe/ContactForm/ContactForm.jsx
@@ -1,23 +1,3 @@
-// import React from 'react'
-// import './ContactForm.css'
-// const ContactForm = () => {
-//   return (
-//     <div className='contact-form-content'>
-//         <form action="">
-//             <div className='name-container'>
-//                 <input type="text" name='firstname' placeholder='Nombre' />
-//                 <input type="text" name='lastname' placeholder='Apellido' />
-//             </div>
-            
-//             <input type="text" name='email' placeholder='Email' />
-//             <textarea name="message" type="text" placeholder='Mensaje' rows={3} style={{ resize: 'none' }}/>
-
-//             <button>Enviar</button>
-//         </form>
-//     </div>
-//   )
-// }
-
 // export default ContactForm
 import React, { useState } from 'react';
 import './ContactForm.css';
